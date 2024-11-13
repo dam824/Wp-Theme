b5c196fab3a30b2d48eb4c611f9ca89f87cd7ed9
dam824 <damiendagory@gmail.com>
2024-11-13 22:00:00 +0100

    finish inte

---
+++ template-parts/sections/accompagnement.php
@@ -0,0 +1,45 @@
+<!-- sections klp accompagne  -->
+<section class="home-about-accompagnement">
+    <div class="accompagnement-title">
+        <h2>KLP Partners vous accompagne<br>
+            <span>partout en France<span>
+        </h2>
+    </div>
+    <div class="accompagnement-content">
+        <div class="accompagnement-content-img">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/accompagnement-photo.png" alt="image tour effeil">
+        </div>
+        <div class="accompagnement-content-list">
+            <ul>
+                <li>
+                    <span>Paris</span>
+                    <a href="/cabinet-paris" class="location-link">
+                        <div class="location-icon">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Arrow_Forward.svg" alt="Arrow Icon">
+                        </div>
+                        Notre cabinet à Paris
+                    </a>
+                </li>
+                <li>
+                    <span>Versailles</span>
+                    <a href="/cabinet-versailles" class="location-link">
+                        <div class="location-icon">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Arrow_Forward.svg">
+                        </div>
+                        Notre cabinet à Versailles
+                    </a>
+                </li>
+                <li>
+                    <span>Marseille</span>
+                    <a href="/cabinet-marseille" class="location-link">
+                        <div class="location-icon">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Arrow_Forward.svg">
+                        </div>
+                        Notre cabinet à Marseille
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
